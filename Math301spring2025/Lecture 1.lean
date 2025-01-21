b636dd5a35c7ae84a13ec546e9371f2af2e3fb2f
tghyde <thyde641@gmail.com>
2025-01-21 15:21:23 -0500

    updated is_continuous in Lecture 1

--- Math301spring2025/Lecture 1.lean
+++ Math301spring2025/Lecture 1.lean
@@ -9,7 +9,7 @@ theorem infinitely_many_primes : ∀ n : ℕ, ∃ p > n, Nat.Prime p := by
 
 -- Define a continuous function f : ℝ → ℝ.
 def is_continuous (f : ℝ → ℝ) : Prop :=
-  ∀ (x : ℝ) (ε : ℝ), ∃ (δ : ℝ), ∀ (y : ℝ), (ε > 0 → δ > 0 → |x - y| < δ → |f x - f y| < ε)
+  ∀ x, ∀ ε > 0, ∃ δ > 0, ∀ y, (|x - y| < δ → |f x - f y| < ε)
 
 -- Prove that the sum of continuous functions is continuous
 theorem sum_of_continuous_is_continuous : ∀ (f : ℝ → ℝ) (g : ℝ → ℝ), is_continuous f →  is_continuous g → is_continuous (f + g) := by
