f8c43cc5c9539b678f405cf613555da3f077b6df
tghyde <thyde641@gmail.com>
2025-01-21 11:35:00 -0500

    updated math301spring2025.lean

--- Math301spring2025.lean
+++ Math301spring2025.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Math301spring2025` library.
 -- Import modules here that should be built as part of the library.
-import Math301spring2025.Basic
+-- import Math301spring2025.Basic
+#eval 2 + 2
